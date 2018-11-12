5a0dc1220146f60c9e1d3a6fedee5f09848ca881
Songseunghyo1 <tmdgy15@gmail.com>
2018-11-12 22:02:09 +0900

    add Project

---
+++ jnucecodefestival/src/main/java/com/example/jnucecodefestival/JnucecodefestivalApplication.java
@@ -0,0 +1,12 @@
+package com.example.jnucecodefestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnucecodefestivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnucecodefestivalApplication.class, args);
+    }
+}
