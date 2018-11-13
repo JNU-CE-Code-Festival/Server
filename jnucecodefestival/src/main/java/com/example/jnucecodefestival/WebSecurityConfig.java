cb39c0014e5fd19ef351a751634dd03c96d6ba43
Songseunghyo1 <tmdgy15@gmail.com>
2018-11-13 21:40:39 +0900

    403 success

--- jnucecodefestival/src/main/java/com/example/jnucecodefestival/WebSecurityConfig.java
+++ jnucecodefestival/src/main/java/com/example/jnucecodefestival/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,7 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/hello").hasRole("USER")
+            .antMatchers("/hello")
+            .hasRole("USER")
             .anyRequest().permitAll()
             .and()
             .formLogin().loginPage("/login")
