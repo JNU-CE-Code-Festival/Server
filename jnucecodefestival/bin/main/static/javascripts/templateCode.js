2d6e6e9b97c5fba4fc1e647b820c99349cb67455
aerain <j40f893@gmail.com>
2018-11-21 20:59:08 +0900

    execute

---
+++ jnucecodefestival/bin/main/static/javascripts/templateCode.js
@@ -0,0 +1,26 @@
+var template = {
+    java: `
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성하세요.
+    }
+}`,
+    c: `
+#include <stdio.h>
+
+int main(void) {
+    // 여기에 코드를 작성하세요.
+    return 0;
+}`,
+    cpp: `
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // 여기에 코드를 작성하세요.
+    return 0;
+}`,
+    js: `
+// 여기에 코드를 작성하세요.`,
+    py: `
+# 여기에 코드를 작성하세요.`
+}
---
+++ jnucecodefestival/bin/main/static/javascripts/textarea.js
@@ -0,0 +1,18 @@
+$(document).delegate('#textbox', 'keydown', function(e) {
+    var keyCode = e.keyCode || e.which;
+  
+    if (keyCode == 9) {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      $(this).val($(this).val().substring(0, start)
+                  + "\t"
+                  + $(this).val().substring(end));
+  
+      // put caret at right position again
+      this.selectionStart =
+      this.selectionEnd = start + 1;
+    }
+});
