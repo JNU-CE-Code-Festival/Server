47aa4f048934c92971b2943aa228ecdd86237ef3
aerain <j40f893@gmail.com>
2018-11-28 07:46:39 +0900

    cpp

--- template/1/cpp/2.cpp
+++ template/1/cpp/2.cpp
@@ -6,12 +6,7 @@ using namespace std;
 string solution(string input);
 int main(int argc, char* argv[]) {
     string str(argv[1]);
-    string answer=solution(str);
+    string answer(solution(str));
     cout << answer << endl;
     return 0;
-}
-
-string solution(string input) {
-    string answer= "babo";
-    return answer;
 }
