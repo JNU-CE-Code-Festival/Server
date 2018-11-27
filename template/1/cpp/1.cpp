df8cf853d63d8640c9ce7953ecaaba22360a1ba5
aerain <j40f893@gmail.com>
2018-11-28 07:41:10 +0900

    cpp test

---
+++ template/1/cpp/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void solution();
+
+int main(void) {
+    solution();
+    return 0;
+}
---
+++ template/1/cpp/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string solution(string input);
+int main(int argc, char* argv[]) {
+    string str(argv[1]);
+    string answer=solution(str);
+    cout << answer << endl;
+    return 0;
+}
+
+string solution(string input) {
+    string answer= "babo";
+    return answer;
+}
